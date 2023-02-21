334bb59ec93d47829e90fba5f9a14235cc9561fd
Morton Jonuschat <mjonuschat@gmail.com>
2023-02-20 17:50:31 -0800

    [FEATURE] Z-Rotate on import

--- src/libslic3r/Preset.cpp
+++ src/libslic3r/Preset.cpp
@@ -556,7 +556,9 @@ static std::vector<std::string> s_Preset_printer_options {
     "max_print_height", "default_print_profile", "inherits",
     "remaining_times", "silent_mode",
     "machine_limits_usage", "thumbnails", "thumbnails_format",
-    "nozzle_high_flow"
+    "nozzle_high_flow",
+    // BOSS
+    "init_z_rotate"
 };
 
 static std::vector<std::string> s_Preset_sla_print_options {
