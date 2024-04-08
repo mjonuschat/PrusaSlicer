352e8ed248ea9defeb4a31e22e89eb4f1e47dbe5
Morton Jonuschat <mjonuschat@gmail.com>
2024-04-08 16:09:04 -0700

    feat(perimeters): Alternate extra perimeter
    This ports the "Alternate extra wall" feature from [Cura](https://support.makerbot.com/s/article/1837650983225) to PrusaSlicer.
    
    This setting adds an extra wall every other layer. This way the infill gets caught between the walls, resulting in:
    
    * Theoretically, stronger prints due to better infill anchoring.
    * Allows for the infill/wall overlap to be set to low values without compromising object strength while maintaining external surface quality.
    * It may contribute to improving water tightness of models as the gaps between the top and bottom layers and the walls are overlapping.
    
    Ported from SuperSlicer (Remi Durand @supermerill)

--- src/libslic3r/PerimeterGenerator.cpp
+++ src/libslic3r/PerimeterGenerator.cpp
@@ -2,6 +2,7 @@
 ///|/ Copyright (c) SuperSlicer 2023 Remi Durand @supermerill
 ///|/ Copyright (c) 2021 Ilya @xorza
 ///|/ Copyright (c) Slic3r 2015 - 2016 Alessandro Ranellucci @alranel
+///|/ Copyright (c) SuperSlicer 2019 Remi Durand @supermerill
 ///|/
 ///|/ PrusaSlicer is released under the terms of the AGPLv3 or higher
 ///|/
@@ -1026,6 +1027,10 @@ void PerimeterGenerator::process_arachne(
     // extra perimeters for each one
     // detect how many perimeters must be generated for this island
     int loop_number = params.config.perimeters + surface.extra_perimeters - 1; // 0-indexed loops
+    // Extra perimeter on odd layers
+    if (params.config.alternate_extra_perimeter && params.config.perimeters > 0 && params.layer_id % 2 == 1 && !params.spiral_vase && params.config.fill_density.value > 0)
+        loop_number++;
+
     if (loop_number > 0 && ((params.config.top_one_perimeter_type == TopOnePerimeterType::TopmostOnly && upper_slices == nullptr) || (params.config.only_one_perimeter_first_layer && params.layer_id == 0)))
         loop_number = 0;
 
@@ -1229,7 +1234,10 @@ void PerimeterGenerator::process_classic(
     // we need to process each island separately because we might have different
     // extra perimeters for each one
     // detect how many perimeters must be generated for this island
-    int        loop_number = params.config.perimeters + surface.extra_perimeters - 1;  // 0-indexed loops
+    int loop_number = params.config.perimeters + surface.extra_perimeters - 1;  // 0-indexed loops
+    // Alternate extra perimeter on odd layers
+    if (params.config.alternate_extra_perimeter && params.config.perimeters > 0 && params.layer_id % 2 == 1 && !params.spiral_vase && params.config.fill_density.value > 0)
+        loop_number++;
 
     // Set the topmost layer to be one perimeter.
     if (loop_number > 0 && ((params.config.top_one_perimeter_type != TopOnePerimeterType::None && upper_slices == nullptr) || (params.config.only_one_perimeter_first_layer && params.layer_id == 0)))
