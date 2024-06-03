d5bf8d729d9b2c3d6684f6f945831ea20bea6cc7
Morton Jonuschat <mjonuschat@gmail.com>
2024-06-02 19:11:04 -0700

    feat: Make first layer and top flow ratio adjustable

--- src/libslic3r/Preset.cpp
+++ src/libslic3r/Preset.cpp
@@ -500,7 +500,7 @@ static std::vector<std::string> s_Preset_print_options {
     "dont_support_bridges", "thick_bridges", "notes", "custom_parameters_print", "complete_objects",
     "gcode_comments", "gcode_label_objects", "output_filename_format", "post_process", "gcode_substitutions", "perimeter_extruder",
     "infill_extruder", "solid_infill_extruder", "support_material_extruder", "support_material_interface_extruder",
-    "ooze_prevention", "standby_temperature_delta", "interface_shells", "extrusion_width", "first_layer_extrusion_width",
+    "ooze_prevention", "standby_temperature_delta", "interface_shells", "extrusion_width", "first_layer_extrusion_width", 
     "perimeter_extrusion_width", "external_perimeter_extrusion_width", "infill_extrusion_width", "solid_infill_extrusion_width",
     "top_infill_extrusion_width", "support_material_extrusion_width", "infill_overlap", "infill_anchor", "infill_anchor_max", "bridge_flow_ratio",
     "elefant_foot_compensation", "xy_size_compensation", "resolution", "gcode_resolution", "arc_fitting",
@@ -571,6 +571,9 @@ static std::vector<std::string> s_Preset_print_options {
     "overhangs_reverse",
     "infill_reverse",
     "internal_perimeters_reverse",
+
+    "first_layer_flow_ratio",
+    "top_layer_flow_ratio",
 };
 
 static std::vector<std::string> s_Preset_filament_options {
