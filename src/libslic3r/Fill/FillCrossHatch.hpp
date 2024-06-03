17ad07d12785c6f31161f2f0493a861e5b9157e6
Morton Jonuschat <mjonuschat@gmail.com>
2024-06-02 20:47:00 -0700

    feat(infill): Add CrossHatch infill pattern
    Ported from OrcaSlicer (Bambu Lab)

---
+++ src/libslic3r/Fill/FillCrossHatch.hpp
@@ -0,0 +1,35 @@
+///|/ Copyright (c) BambuStudio 2023 Bambu Lab
+///|/
+///|/ PrusaSlicer is released under the terms of the AGPLv3 or higher
+///|/
+#ifndef slic3r_FillCrossHatch_hpp_
+#define slic3r_FillCrossHatch_hpp_
+
+#include <map>
+
+#include "../libslic3r.h"
+
+#include "FillBase.hpp"
+
+namespace Slic3r {
+
+class FillCrossHatch : public Fill
+{
+public:
+    Fill *clone() const override { return new FillCrossHatch(*this); };
+    ~FillCrossHatch() override {}
+
+    bool is_self_crossing() override { return false; }
+
+protected:
+	void _fill_surface_single(
+	    const FillParams                &params, 
+	    unsigned int                     thickness_layers,
+	    const std::pair<float, Point>   &direction, 
+	    ExPolygon                 		 expolygon,
+	    Polylines                       &polylines_out) override;
+};
+
+} // namespace Slic3r
+
+#endif // slic3r_FillCrossHatch_hpp_
