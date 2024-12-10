45328e64a7e80c34db19eb226b6e8a851c8a8d07
enricoturri1966 <enrico.turri@prusa3d.cz>
2024-12-10 10:11:23 +0100

    Rectangle selection - draw rectangle in scene

--- src/slic3r-shared/include/Slic3r/App/Plater/QuickSelectGizmo.hpp
+++ src/slic3r-shared/include/Slic3r/App/Plater/QuickSelectGizmo.hpp
@@ -11,6 +11,52 @@
 
 namespace Slic3r::App::Plater {
 
+class RectangleSelection
+{
+public:
+    enum class Type : uint8_t
+    {
+        Undefined,
+        Replace,
+        Add,
+        Remove
+    };
+
+    RectangleSelection(const Render::ScreenInfo& screen_info, Render::Device& device)
+        : m_screen_info(screen_info)
+        , m_device(device)
+        , m_geometry(device, Render::BufferUsage::DynamicDraw)
+    {}
+
+    void activate(Type type, const Vec2i& initial_mouse_pos) {
+        m_active = true;
+        m_type = type;
+        m_initial_mouse_pos = initial_mouse_pos;
+    }
+
+    void deactivate() {
+        m_active = false;
+        m_defined = false;
+    }
+
+    [[nodiscard]] bool is_active() { return m_active; }
+
+    void update(const Vec2i& curr_mouse_pos);
+    [[nodiscard]] bool update_selection(SelectionHandler& selection_handler);
+
+    void render(Render::CommandBuffer& cmd_buffer);
+
+private:
+    const Render::ScreenInfo& m_screen_info;
+    Render::Device& m_device;
+
+    Type m_type{ Type::Undefined };
+    bool m_active{ false };
+    bool m_defined{ false };
+    Vec2i m_initial_mouse_pos;
+    Render::Geometry m_geometry;
+};
+
 class QuickSelectGizmo : public IGizmo {
 public:
     QuickSelectGizmo(
@@ -19,10 +65,8 @@ public:
         const Render::ScreenInfo& screen_info
     )
         : m_scene_interactor(scene_interactor)
-        , m_device(device)
         , m_selection_handler(scene_interactor)
-        , m_selection_rect(device, Render::BufferUsage::DynamicDraw)
-        , m_screen_info(screen_info)
+        , m_rectangle_selection(screen_info, device)
     {}
 
     GizmoActivationState on_mouse(const GizmoEventContext& ctx, bool only_active) override;
@@ -31,22 +75,16 @@ public:
     void render_scene(Render::CommandBuffer& cmd_buffer) override;
 
 private:
-    void update_selection_rect(float x, float y, float w, float h);
-
-private:
     using Clock = std::chrono::steady_clock;
     using TimePoint = std::chrono::time_point<Clock>;
 
     Biz::Scene::SceneInteractor& m_scene_interactor;
-    Render::Device& m_device;
-    const Render::ScreenInfo& m_screen_info;
     SelectionHandler m_selection_handler;
     TimePoint m_click_start;
 
-    Render::Geometry m_selection_rect;
-    bool m_selection_rect_shown{false};
-
     bool m_processing{false};
+
+    RectangleSelection m_rectangle_selection;
 };
 
 }
--- src/slic3r-shared/src/Slic3r/App/Plater/QuickSelectGizmo.cpp
+++ src/slic3r-shared/src/Slic3r/App/Plater/QuickSelectGizmo.cpp
@@ -6,10 +6,79 @@
 
 namespace Slic3r::App::Plater {
 
+void RectangleSelection::update(const Vec2i& curr_mouse_pos)
+{
+    Render::Rect rect{
+        std::min(m_initial_mouse_pos.x(), curr_mouse_pos.x()),
+        std::min(m_initial_mouse_pos.y(), curr_mouse_pos.y()),
+        std::abs(curr_mouse_pos.x() - m_initial_mouse_pos.x()),
+        std::abs(curr_mouse_pos.y() - m_initial_mouse_pos.y())
+    };
+
+    float scr_w = m_screen_info.logical_width();
+    float scr_h = m_screen_info.logical_height();
+
+    ASSERT(scr_w > 0.0f && scr_h > 0.0f);
+
+    float left   = 2.0f * (rect.x / scr_w - 0.5f);
+    float right  = 2.0f * ((rect.x + rect.width) / scr_w - 0.5f);
+    float top    = -2.0f * (rect.y / scr_h - 0.5f);
+    float bottom = -2.0f * ((rect.y + rect.height) / scr_h - 0.5f);
+
+    Render::GeometryBuilder<Render::VertexP3> builder;
+    auto* shader = m_device.context().shader_manager().get_shader("flat");
+    ASSERT(shader != nullptr);
+    builder
+        .add_vertex({ {left,  bottom, 0.0f} })
+        .add_vertex({ {right, bottom, 0.0f} })
+        .add_vertex({ {right, top,    0.0f} })
+        .add_vertex({ {left,  top,    0.0f} })
+        .add_draw_command({ Render::PrimitiveType::LineLoop, 0, 4, Render::Material{}.set_shader(shader) });
+    builder.update(m_geometry);
+
+    m_defined = m_initial_mouse_pos != curr_mouse_pos;
+}
+
+bool RectangleSelection::update_selection(SelectionHandler& selection_handler)
+{
+    if (!(m_active && m_defined))
+        return false;
+
+    return true;
+}
+
+void RectangleSelection::render(Render::CommandBuffer& cmd_buffer)
+{
+    if (!(m_active && m_defined))
+        return;
+
+    ColorRGBA color;
+    switch (m_type)
+    {
+    case Type::Replace:
+    case Type::Add:     { color = ColorRGBA(0.3f, 1.0f, 0.3f, 1.0f); break; }
+    case Type::Remove:  { color = ColorRGBA(1.0f, 0.3f, 0.3f, 1.0f); break; }
+    default:            { color = ColorRGBA(0.0f, 0.0f, 0.0f, 1.0f); break; }
+    }
+
+    Render::Material mat;
+    Matrix4f vm = Matrix4f::Identity();
+    mat
+        .set_uniform("view_model_matrix", vm)
+        .set_uniform("projection_matrix", vm)
+        .set_uniform("uniform_color", color);
+    cmd_buffer.bind_and_draw(m_geometry, mat);
+}
+
 GizmoActivationState QuickSelectGizmo::on_mouse(const GizmoEventContext& ctx, bool only_active)
 {
     using namespace std::chrono_literals;
-    const auto type = ctx.mouse_event().type();
+    const auto& evt = ctx.mouse_event();
+    auto type = evt.type();
+
+    bool shift_down = (evt.key_modifiers() & Platform::KeyModifiers(Platform::KeyModifier::Shift)) != 0;
+    bool ctrl_down  = (evt.key_modifiers() & Platform::KeyModifiers(Platform::KeyModifier::Ctrl)) != 0;
+    bool alt_down   = (evt.key_modifiers() & Platform::KeyModifiers(Platform::KeyModifier::Alt)) != 0;
 
     constexpr static Clock::duration max_click_duration = Clock::duration {500ms};
 
@@ -20,31 +89,54 @@ GizmoActivationState QuickSelectGizmo::on_mouse(const GizmoEventContext& ctx, bo
         });
 
     if (type == Platform::MouseEvent::Type::ButtonDown) {
-        if (ctx.mouse_event().button() != Platform::MouseButton::Left) {
+        if (evt.button() != Platform::MouseButton::Left) {
             m_processing = false;
             return GizmoActivationState::Inactive;
         }
 
-        m_click_start = Clock::now();
-        m_processing = true;
+        RectangleSelection::Type rect_sel_type = (shift_down && ctrl_down) ? RectangleSelection::Type::Add :
+                                                 shift_down ? RectangleSelection::Type::Replace :
+                                                 alt_down ? RectangleSelection::Type::Remove :
+                                                 RectangleSelection::Type::Undefined;
+        if (rect_sel_type != RectangleSelection::Type::Undefined)
+            m_rectangle_selection.activate(rect_sel_type, { evt.x(), evt.y() });
+
+        if (m_rectangle_selection.is_active())
+            return GizmoActivationState::Active;
+        else {
+            m_click_start = Clock::now();
+            m_processing = true;
+        }
 
         return only_active ? GizmoActivationState::Active : GizmoActivationState::Probing;
     }
 
-    if (m_processing && Clock::now() - m_click_start >= max_click_duration) {
-        m_processing = false;
-        SPDLOG_INFO("QuickSelectGizmo activation timed out");
+    if (!m_rectangle_selection.is_active()) {
+        if (m_processing && Clock::now() - m_click_start >= max_click_duration) {
+            m_processing = false;
+            SPDLOG_INFO("QuickSelectGizmo activation timed out");
+        }
+        if (!m_processing)
+            return GizmoActivationState::Inactive;
     }
-    if (!m_processing)
-        return GizmoActivationState::Inactive;
 
     if (type == Platform::MouseEvent::Type::Move) {
-        return  GizmoActivationState::Probing;
+        if (m_rectangle_selection.is_active()) {
+            m_rectangle_selection.update({ evt.x(), evt.y() });
+            return GizmoActivationState::Active;
+        }
+        else
+            return GizmoActivationState::Probing;
     } else if (type == Platform::MouseEvent::Type::ButtonUp) {
-        const bool additive = (ctx.mouse_event().key_modifiers() &
-                               Platform::KeyModifiers(Platform::KeyModifier::Shift)) != 0;
-        const auto& selection = m_scene_interactor.selection();
+        if (m_rectangle_selection.is_active()) {
+            bool res = m_rectangle_selection.update_selection(m_selection_handler);
+            m_rectangle_selection.deactivate();
+            if (res)
+                return GizmoActivationState::Done;
+        }
 
+        const bool additive = shift_down;
+        const auto& selection = m_scene_interactor.selection();
         if (selection.empty()) {
             if (it == ctx.pick_results().end())
                 return GizmoActivationState::Inactive;
@@ -77,38 +169,16 @@ GizmoActivationState QuickSelectGizmo::on_mouse(const GizmoEventContext& ctx, bo
             return GizmoActivationState::Done;
         }
     }
+    else if (evt.type() == Platform::MouseEvent::Type::Leave) {
+        if (m_rectangle_selection.is_active())
+            return GizmoActivationState::Active;
+    }
     return GizmoActivationState::Inactive;
 }
 
-void QuickSelectGizmo::update_selection_rect(float x, float y, float w, float h)
-{
-    Render::GeometryBuilder<Render::VertexP3> builder;
-
-
-    auto* shader = m_device.context().shader_manager().get_shader("dashed_thick_lines");
-
-    builder
-        .add_vertex({{x, y, 0}})
-        .add_vertex({{x + w, y, 0}})
-        .add_vertex({{x + w, y + h, 0}})
-        .add_vertex({{x, y + h, 0}})
-    .add_draw_command({Render::PrimitiveType::LineLoop, 0, 4, Render::Material{}.set_shader(shader)});
-    builder.update(m_selection_rect);
-}
-
-
 void QuickSelectGizmo::render_scene(Render::CommandBuffer& cmd_buffer)
 {
-    if (!m_selection_rect_shown)
-        return;
-
-    Render::Material mat;
-    Matrix4f vm = Matrix4f::Identity();
-    // TODO: setup ortho projection (use m_screen_info to get physical size---i.e. in real pixels)
-    mat
-        .set_uniform("view_model_matrix", vm)
-        .set_uniform("projection_matrix", Matrix4f{});
-    cmd_buffer.bind_and_draw(m_selection_rect, mat);
+    m_rectangle_selection.render(cmd_buffer);
 }
 
 } // namespace Slic3r::App::Plater
