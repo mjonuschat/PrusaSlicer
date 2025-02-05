57d5618533c540b455878b9ad0d3f0782a17f462
Jan Bařtipán <barzto@gmail.com>
2025-02-05 20:12:23 +0100

    Tests migrated to Catch2 v3

--- src/slic3r-shared/test/Slic3r/App/Scene/Node.cpp
+++ src/slic3r-shared/test/Slic3r/App/Scene/Node.cpp
@@ -1,4 +1,6 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 #include "Slic3r/App/Scene/Scene.hpp"
 
