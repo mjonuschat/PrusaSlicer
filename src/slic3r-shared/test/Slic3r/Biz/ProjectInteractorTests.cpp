8cbdc496447f9c8ad71b273e3312707f649e9c02
Jan Bařtipán <barzto@gmail.com>
2025-02-05 20:12:23 +0100

    Catch2 updated to v3.8, all tests migrated

--- src/slic3r-shared/test/Slic3r/Biz/ProjectInteractorTests.cpp
+++ src/slic3r-shared/test/Slic3r/Biz/ProjectInteractorTests.cpp
@@ -1,6 +1,6 @@
 #include "Slic3r/Biz/IProjectsChangedListener.hpp"
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #include "Slic3r/Biz/ProjectInteractor.hpp"
 #include "libslic3r/Model.hpp"
