91855256b5b28ccddf438021441fe9a006ff4783
Nikita Vanku <nikita.vanku@prusa3d.cz>
2025-04-04 13:56:30 +0200

    CMake 4 & GCC 14.2 fixes
    Update SDL to 2.32.4
        Compilation failed in the older version on Linux with GCC 14+ if pipewire is used.
    
    Comment out block containing undefined variable
    
    Force autoreconf in MPFR
        If system uses different automake version a MPFR configure script fails.
        Forcing autoreconf will fix this.
        Fixes #14081
    
    Force -Wno-error=stringop-overflow GCC flag for oneTBB compilation
        Compilation of oneTBB under GCC 14+ otherwise fails due to these warnings.
        This is a reccuring issue in oneTBB repository, forcing this flag should
        save us a lot of headache in the future.
    
    Replace boost_headersonly with boost_libs private dependency to admesh
        admesh code uses Boost::log but it was never linked

--- deps/+SDL/SDL.cmake
+++ deps/+SDL/SDL.cmake
@@ -1,8 +1,8 @@
 include(GNUInstallDirs)
 add_cmake_project(
         SDL
-        URL https://github.com/libsdl-org/SDL/releases/download/release-2.30.2/SDL2-2.30.2.zip
-        URL_HASH SHA256=09a822abf6e97f80d09cf9c46115faebb3476b0d56c1c035aec8ec3f88382ae7
+        URL https://github.com/libsdl-org/SDL/releases/download/release-2.32.4/SDL2-2.32.4.zip
+        URL_HASH SHA256=030bd2768653bf11cefef06aaa99e61e850326871d283b81d324fb7242b6c702
         CMAKE_ARGS
             -DINCLUDE_INSTALL_DIR=${CMAKE_INSTALL_INCLUDEDIR}
             -DBUILD_SHARED_LIBS=ON
--- deps/+TBB/TBB.cmake
+++ deps/+TBB/TBB.cmake
@@ -1,3 +1,10 @@
+# Force -Wno-error=stringop-overflow if GCC compiler is used
+# otherwise oneTBB compilation fails on GCC 14+
+set(_tbb_cxx_flags "")
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(_tbb_cxx_flags "-Wno-error=stringop-overflow")
+endif()
+
 add_cmake_project(
     TBB
     URL "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.12.0.zip"
@@ -8,6 +15,5 @@ add_cmake_project(
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
         -DCMAKE_DEBUG_POSTFIX=_debug
         -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+        -DCMAKE_CXX_FLAGS=${_tbb_cxx_flags}
 )
-
-
