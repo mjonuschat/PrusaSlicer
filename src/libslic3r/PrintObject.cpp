92acbe45d3009316e06bc38511be1c887e141e79
Morton Jonuschat <mjonuschat@gmail.com>
2025-05-03 14:03:56 -0700

    feat(perimeters): Minimum hole size for external perimeters first

--- src/libslic3r/PrintObject.cpp
+++ src/libslic3r/PrintObject.cpp
@@ -736,6 +736,7 @@ bool PrintObject::invalidate_state_by_config_options(
             || opt_key == "infill_overlap"
             || opt_key == "external_perimeters_first"
             || opt_key == "external_perimeters_first_holes"
+            || opt_key == "external_perimeters_first_holes_min_size"
             || opt_key == "arc_fitting"
             || opt_key == "top_one_perimeter_type"
             || opt_key == "only_one_perimeter_first_layer"
