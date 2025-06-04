a95f88e594ad396df79f4e5e5060a933bafd605b
Morton Jonuschat <mjonuschat@gmail.com>
2025-06-03 20:05:15 -0700

    feat: Disable external perimeters first for initial layer(s)

--- src/libslic3r/PrintObject.cpp
+++ src/libslic3r/PrintObject.cpp
@@ -732,6 +732,7 @@ bool PrintObject::invalidate_state_by_config_options(
             || opt_key == "external_perimeters_first"
             || opt_key == "external_perimeters_first_holes"
             || opt_key == "external_perimeters_first_holes_min_size"
+            || opt_key == "external_perimeters_first_disabled_first_layers"
             || opt_key == "arc_fitting"
             || opt_key == "alternate_extra_perimeter"
             || opt_key == "overhangs_reverse"
