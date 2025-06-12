8daf18d4f899b9cd189991aa8c46f27788991551
Jan Bařtipán <barzto@gmail.com>
2025-06-12 13:49:02 +0200

    updated deps to build with recent Xcode 16.4: - Blosc.cmake: deactive SSE2 as its support is dropped - CGAL: 5.6.1 -> 5.6.2 - z3: 4.13.0 -> 4.15.1 - PNG: patched `pngpriv.h` to only include <math.h> instead of <fp.h> in some cases

--- deps/+CGAL/CGAL.cmake
+++ deps/+CGAL/CGAL.cmake
@@ -1,7 +1,7 @@
 add_cmake_project(
     CGAL
-    URL      https://github.com/CGAL/cgal/archive/refs/tags/v5.6.1.zip
-    URL_HASH SHA256=a968cc77b9a2c6cbe5b1680ceee8d8cd8c5369aedb9daced9e5c90b4442dc574
+    URL      https://github.com/CGAL/cgal/archive/refs/tags/v5.6.2.zip
+    URL_HASH SHA256=29acaeee5a76a95029fac23131bb1c3a4a75df9a0e7e43b465a1f32d0628f45d
 )
 
 include(GNUInstallDirs)
