d9e89cf56436874432f775ecceff59b56410dee2
Martin Šach <martin.sachin@gmail.com>
2025-07-01 14:55:11 +0200

    Remove SLA import
    It is not compatible with the current configs anyway.

--- src/libslic3r/src/libslic3r/Format/SL1.cpp
+++ src/libslic3r/src/libslic3r/Format/SL1.cpp
@@ -12,24 +12,11 @@
 #include "Slic3r/Time.hpp"
 #include "libslic3r/Utils.hpp"
 #include "libslic3r/Zipper.hpp"
-#include "libslic3r/SLAPrint.hpp"
-#include "Slic3r/Exception.hpp"
-#include "libslic3r/MTUtils.hpp"
-#include "libslic3r/PrintConfig.hpp"
 
 #include "libslic3r/miniz_extension.hpp" // IWYU pragma: keep
 #include <LocalesUtils.hpp>
 #include "libslic3r/GCode/ThumbnailData.hpp"
 #include "libslic3r/Utils/JsonUtils.hpp"
-
-#include "SLAArchiveReader.hpp"
-#include "SLAArchiveFormatRegistry.hpp"
-#include "ZipperArchiveImport.hpp"
-
-#include "libslic3r/MarchingSquares.hpp"
-#include "libslic3r/PNGReadWrite.hpp"
-#include "libslic3r/ClipperUtils.hpp"
-#include "Slic3r/Biz/Algorithms/Execution/ExecutionTBB.hpp"
 
 #include "libslic3r/SLA/RasterBase.hpp"
 
@@ -43,7 +30,6 @@ using namespace Slic3r::Biz::Slicing;
 
 namespace Slic3r {
 
-namespace execution = Slic3r::Biz::Algorithms::Execution;
 using ConfMap = std::map<std::string, std::string>;
 using Domain::EnumVectorWrapper;
 
@@ -311,212 +297,8 @@ public:
         return std::move(encoded_raster.m_buffer);
     }
 };
-}
 
-std::unique_ptr<Slic3r::ISlaRasterizer> Slic3r::create_sl1_rasterizer(const SLAPrintConfigView& cfg){
+std::unique_ptr<Slic3r::ISlaRasterizer> create_sl1_rasterizer(const SLAPrintConfigView& cfg){
     return std::make_unique<Sl1Rasterizer>(cfg);
 }
-
-// /////////////////////////////////////////////////////////////////////////////
-// Reader implementation
-// /////////////////////////////////////////////////////////////////////////////
-
-namespace marchsq {
-
-template<> struct _RasterTraits<Slic3r::png::ImageGreyscale> {
-    using Rst = Slic3r::png::ImageGreyscale;
-
-       // The type of pixel cell in the raster
-    using ValueType = uint8_t;
-
-       // Value at a given position
-    static uint8_t get(const Rst &rst, size_t row, size_t col)
-    {
-        return rst.get(row, col);
-    }
-
-       // Number of rows and cols of the raster
-    static size_t rows(const Rst &rst) { return rst.rows; }
-    static size_t cols(const Rst &rst) { return rst.cols; }
-};
-
-} // namespace marchsq
-
-namespace Slic3r {
-
-template<class Fn> static void foreach_vertex(ExPolygon &poly, Fn &&fn)
-{
-    for (auto &p : poly.contour.points) fn(p);
-    for (auto &h : poly.holes)
-        for (auto &p : h.points) fn(p);
-}
-
-void invert_raster_trafo(ExPolygons &                  expolys,
-                         const sla::RasterBase::Trafo &trafo,
-                         coord_t                       width,
-                         coord_t                       height)
-{
-    if (trafo.flipXY) std::swap(height, width);
-
-    for (auto &expoly : expolys) {
-        if (trafo.mirror_y)
-            foreach_vertex(expoly, [height](Point &p) {p.y() = height - p.y(); });
-
-        if (trafo.mirror_x)
-            foreach_vertex(expoly, [width](Point &p) {p.x() = width - p.x(); });
-
-        expoly.translate(-trafo.center_x, -trafo.center_y);
-
-        if (trafo.flipXY)
-            foreach_vertex(expoly, [](Point &p) { std::swap(p.x(), p.y()); });
-
-        if ((trafo.mirror_x + trafo.mirror_y + trafo.flipXY) % 2) {
-            expoly.contour.reverse();
-            for (auto &h : expoly.holes) h.reverse();
-        }
-    }
-}
-
-RasterParams get_raster_params(const DynamicPrintConfig &cfg)
-{
-    auto *opt_disp_cols = cfg.option<ConfigOptionInt>("display_pixels_x");
-    auto *opt_disp_rows = cfg.option<ConfigOptionInt>("display_pixels_y");
-    auto *opt_disp_w    = cfg.option<ConfigOptionFloat>("display_width");
-    auto *opt_disp_h    = cfg.option<ConfigOptionFloat>("display_height");
-    auto *opt_mirror_x  = cfg.option<ConfigOptionBool>("display_mirror_x");
-    auto *opt_mirror_y  = cfg.option<ConfigOptionBool>("display_mirror_y");
-    auto *opt_orient    = cfg.option<ConfigOptionEnum<SLADisplayOrientation>>("display_orientation");
-
-    if (!opt_disp_cols || !opt_disp_rows || !opt_disp_w || !opt_disp_h ||
-        !opt_mirror_x || !opt_mirror_y || !opt_orient)
-        throw MissingProfileError("Invalid SL1 / SL1S file");
-
-    RasterParams rstp;
-
-    rstp.px_w = opt_disp_w->value / (opt_disp_cols->value - 1);
-    rstp.px_h = opt_disp_h->value / (opt_disp_rows->value - 1);
-
-    rstp.trafo = sla::RasterBase::Trafo{opt_orient->value == sladoLandscape ?
-                                            sla::RasterBase::roLandscape :
-                                            sla::RasterBase::roPortrait,
-                                        {opt_mirror_x->value, opt_mirror_y->value}};
-
-    rstp.height = scaled(opt_disp_h->value);
-    rstp.width  = scaled(opt_disp_w->value);
-
-    return rstp;
-}
-
-namespace {
-
-ExPolygons rings_to_expolygons(const std::vector<marchsq::Ring> &rings,
-                               double px_w, double px_h)
-{
-    auto polys = reserve_vector<ExPolygon>(rings.size());
-
-    for (const marchsq::Ring &ring : rings) {
-        Polygon poly; Points &pts = poly.points;
-        pts.reserve(ring.size());
-
-        for (const marchsq::Coord &crd : ring)
-            pts.emplace_back(scaled(crd.c * px_w), scaled(crd.r * px_h));
-
-        polys.emplace_back(poly);
-    }
-
-    // TODO: Is a union necessary?
-    return union_ex(polys);
-}
-
-std::vector<ExPolygons> extract_slices_from_sla_archive(
-    ZipperArchive           &arch,
-    const RasterParams      &rstp,
-    const marchsq::Coord    &win,
-    std::function<bool(int)> progr)
-{
-    std::vector<ExPolygons> slices(arch.entries.size());
-
-    struct Status
-    {
-        double                                 incr, val, prev;
-        bool                                   stop  = false;
-        execution::SpinningMutex<execution::ExecutionTBB> mutex = {};
-    } st{100. / slices.size(), 0., 0.};
-
-    execution::for_each(
-        execution::ex_tbb, size_t(0), arch.entries.size(),
-        [&arch, &slices, &st, &rstp, &win, progr](size_t i) {
-            // Status indication guarded with the spinlock
-            {
-                std::lock_guard lck(st.mutex);
-                if (st.stop) return;
-
-                st.val += st.incr;
-                double curr = std::round(st.val);
-                if (curr > st.prev) {
-                    st.prev = curr;
-                    st.stop = !progr(int(curr));
-                }
-            }
-
-            png::ImageGreyscale img;
-            png::ReadBuf        rb{arch.entries[i].buf.data(),
-                            arch.entries[i].buf.size()};
-            if (!png::decode_png(rb, img)) return;
-
-            constexpr uint8_t isoval = 128;
-            auto              rings = marchsq::execute(img, isoval, win);
-            ExPolygons        expolys = rings_to_expolygons(rings, rstp.px_w,
-                                                            rstp.px_h);
-
-            // Invert the raster transformations indicated in the profile metadata
-            invert_raster_trafo(expolys, rstp.trafo, rstp.width, rstp.height);
-
-            slices[i] = std::move(expolys);
-        },
-        execution::max_concurrency(execution::ex_tbb));
-
-    if (st.stop) slices = {};
-
-    return slices;
-}
-
-} // namespace
-
-ConfigSubstitutions SL1Reader::read(std::vector<ExPolygons> &slices,
-                                    DynamicPrintConfig      &profile_out)
-{
-    std::array<int, 2> windowsize;
-
-    switch(m_quality)
-    {
-    case SLAImportQuality::Fast: windowsize = {8, 8}; break;
-    case SLAImportQuality::Balanced: windowsize = {4, 4}; break;
-    default:
-    case SLAImportQuality::Accurate:
-        windowsize = {2, 2}; break;
-    };
-
-    // Ensure minimum window size for marching squares
-    windowsize[0] = std::max(2, windowsize[0]);
-    windowsize[1] = std::max(2, windowsize[1]);
-
-    std::vector<std::string> includes = { "ini", "png"};
-    std::vector<std::string> excludes = { "thumbnail" };
-    ZipperArchive arch = read_zipper_archive(m_fname, includes, excludes);
-    auto [profile_use, config_substitutions] = extract_profile(arch, profile_out);
-
-    RasterParams   rstp = get_raster_params(profile_use);
-    marchsq::Coord win  = {windowsize[1], windowsize[0]};
-    slices = extract_slices_from_sla_archive(arch, rstp, win, m_progr);
-
-    return std::move(config_substitutions);
-}
-
-ConfigSubstitutions SL1Reader::read(DynamicPrintConfig &out)
-{
-    ZipperArchive arch = read_zipper_archive(m_fname, {"ini"}, {"png", "thumbnail"});
-    return out.load(arch.profile, ForwardCompatibilitySubstitutionRule::Enable);
-}
-
 } // namespace Slic3r
--- src/libslic3r/src/libslic3r/Format/SL1.hpp
+++ src/libslic3r/src/libslic3r/Format/SL1.hpp
@@ -8,9 +8,6 @@
 #include <string>
 #include <memory>
 
-#include "SLAArchiveReader.hpp"
-
-#include "libslic3r/PrintConfig.hpp" // SLAPrinterConfig
 #include "libslic3r/SLA/RasterBase.hpp" // ISlaRasterizer
 #include "libslic3r/SLA/SLAResult.hpp"
 
@@ -31,42 +28,6 @@ void store_sl1(const std::string& file_path, const Biz::Slicing::SLAResult& data
 /// <param name="cfg">Printer configuration like resolution, dimension etc</param>
 /// <returns>Copyable(for each layer) rasterizer</returns>
 std::unique_ptr<ISlaRasterizer> create_sl1_rasterizer(const SLAPrintConfigView& cfg);
-
-class SL1Reader: public SLAArchiveReader {
-    SLAImportQuality m_quality = SLAImportQuality::Balanced;
-    std::function<bool(int)> m_progr;
-    std::string m_fname;
-    
-public:
-    // If the profile is missing from the archive (older PS versions did not have
-    // it), profile_out's initial value will be used as fallback. profile_out will be empty on
-    // function return if the archive did not contain any profile.
-    ConfigSubstitutions read(std::vector<ExPolygons> &slices,
-                             DynamicPrintConfig      &profile_out) override;
-
-    ConfigSubstitutions read(DynamicPrintConfig &profile) override;
-
-    SL1Reader() = default;
-    SL1Reader(const std::string       &fname,
-              SLAImportQuality         quality,
-              std::function<bool(int)> progr)
-        : m_quality(quality), m_progr(progr), m_fname(fname)
-    {}
-};
-
-struct RasterParams {
-    sla::RasterBase::Trafo trafo; // Raster transformations
-    coord_t        width, height; // scaled raster dimensions (not resolution)
-    double         px_h, px_w;    // pixel dimesions
-};
-
-RasterParams get_raster_params(const DynamicPrintConfig &cfg);
-
-void invert_raster_trafo(ExPolygons &                  expolys,
-                         const sla::RasterBase::Trafo &trafo,
-                         coord_t                       width,
-                         coord_t                       height);
-
 } // namespace Slic3r
 
 #endif // ARCHIVETRAITS_HPP
