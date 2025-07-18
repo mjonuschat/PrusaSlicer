0bb4732c0bd649c3deb8bcb00b6dff41819e1716
Morton Jonuschat <mjonuschat@gmail.com>
2025-07-17 19:56:00 -0700

    feat: Set max speed for wipe tower

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -1803,6 +1803,7 @@ void TabPrint::build()
         optgroup->append_single_option_line("wipe_tower_cone_angle");
         optgroup->append_single_option_line("wipe_tower_extra_spacing");
         optgroup->append_single_option_line("wipe_tower_extra_flow");
+        optgroup->append_single_option_line("wipe_tower_max_purge_speed");
         optgroup->append_single_option_line("wipe_tower_no_sparse_layers");
         optgroup->append_single_option_line("single_extruder_multi_material_priming");
         optgroup->append_single_option_line("wipe_tower_disable_cooling_moves");
