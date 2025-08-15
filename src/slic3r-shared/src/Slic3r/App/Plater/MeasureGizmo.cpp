6749690d103d4685b74d53e7130275b836a4a9fb
Enrico Turri <34307919+enricoturri1966@users.noreply.github.com>
2025-08-15 14:30:04 +0200

    Measure gizmo - Fixed broken selection of objects in the scene

--- src/slic3r-shared/src/Slic3r/App/Plater/MeasureGizmo.cpp
+++ src/slic3r-shared/src/Slic3r/App/Plater/MeasureGizmo.cpp
@@ -260,7 +260,8 @@ Scene::GizmoActivationState MeasureGizmo::on_mouse(Scene::GizmoEventContext& ctx
         feature_cache.current.reset();
         update_current_feature_on_scene();
         update_ui_dialog();
-        return return_value;
+        // return Inactive to allow selection of objects in the scene
+        return Scene::GizmoActivationState::Inactive;
     }
 
     feature_cache.hover_id = HoverID::None;
