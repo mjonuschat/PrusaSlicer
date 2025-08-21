74e5004840fccb18270d321ce58bad5980f193b9
Nikita Vanku <nikita.vanku@prusa3d.cz>
2025-08-21 12:57:39 +0200

    SPE-2855: Update naming of project tabs
    First new project won't have number, succeding project will do:
    New Project, New Project (1), New Project (2) etc.

--- src/slic3r-shared/src/Slic3r/App/Yoga/ProjectButton.cpp
+++ src/slic3r-shared/src/Slic3r/App/Yoga/ProjectButton.cpp
@@ -6,8 +6,7 @@
 #include "Slic3r/App/Yoga/LayoutButton.hpp"
 #include "Slic3r/App/I18N/I18N.hpp"
 
-#include "libslic3r/format.hpp"
-
+#include <fmt/format.h>
 #include <imgui/imgui_internal.h>
 
 using namespace Slic3r::Biz;
@@ -15,12 +14,13 @@ using namespace Slic3r::Biz;
 namespace Slic3r::App::Yoga {
 
 ProjectButton::ProjectButton(
-    size_t index, const Domain::SelectionId& data, Biz::ProjectInteractor& project_interactor
-)
-    : AbstractButton("")
-    , Biz::DataObserver<Domain::SelectionId>(index, data)
-    , m_selected_project_changed_listener_scope(project_interactor, *this)
-    , m_project_interactor(project_interactor)
+    size_t index,
+    const Domain::SelectionId& data,
+    Biz::ProjectInteractor& project_interactor
+) :
+    Biz::DataObserver<Domain::SelectionId>(index, data),
+    m_selected_project_changed_listener_scope(project_interactor, *this),
+    m_project_interactor(project_interactor)
 {
     set_allow_overlap(true);
 
@@ -31,8 +31,19 @@ ProjectButton::ProjectButton(
     m_background->set_fill(GImGui->Style.Colors[ImGuiCol_WindowBg]);
 
     const boost::filesystem::path proj_path(m_project_interactor.get_project_name(*m_state));
-    const std::string btn_label = proj_path.empty() ? format("%1% (%2%)", _u8L("New Project"), *m_state) : proj_path.filename().string();
-    const std::string btn_tooltip = proj_path.empty() ? format("%1% (%2%)", _u8L("New Project"), *m_state) : proj_path.string();
+    std::string btn_label;
+    std::string btn_tooltip;
+    if (proj_path.empty()) {
+        std::string new_project = _u8L("New Project");
+        if (*m_state) {
+            new_project += fmt::format(" ({})", *m_state);
+        }
+        btn_label   = new_project;
+        btn_tooltip = new_project;
+    } else {
+        btn_label   = proj_path.filename().string();
+        btn_tooltip = proj_path.string();
+    }
 
     set_tooltip(btn_tooltip);
     set_tooltip_position(Position::Bottom);
@@ -44,7 +55,9 @@ ProjectButton::ProjectButton(
     m_cross->set_max_size({20.f, 20.f});
     m_cross->set_self_align(YGAlignCenter);
     m_cross->set_background_color(IM_COL32_BLACK_TRANS);
-    m_cross->callbacks().action = [this]() { m_project_interactor.remove_project(*m_state); };
+    m_cross->callbacks().action = [this]() {
+        m_project_interactor.remove_project(*m_state);
+    };
 
     callbacks().action = [this]() {
         // Ignore action, if cross button was clicked or if button is already selected
@@ -58,9 +71,15 @@ ProjectButton::ProjectButton(
     set_selected(m_project_interactor.selected_project_id() == data);
 }
 
-bool ProjectButton::is_cross_hovered() const { return m_cross->hovered(); }
+bool ProjectButton::is_cross_hovered() const
+{
+    return m_cross->hovered();
+}
 
-bool ProjectButton::is_selected() { return m_selected; }
+bool ProjectButton::is_selected()
+{
+    return m_selected;
+}
 
 void ProjectButton::set_selected(bool selected)
 {
@@ -69,21 +88,22 @@ void ProjectButton::set_selected(bool selected)
         m_background->set_mode(
             selected ? ProjectButtonBackground::Border : ProjectButtonBackground::FilledRect
         );
-        m_label->set_text_color(
-            GImGui->Style.Colors[selected ? ImGuiCol_Text : ImGuiCol_TextDisabled]
-        );
-        m_label->set_font_type(
-            selected ? Render::ImguiFontType::Bold : Render::ImguiFontType::Regular
-        );
+        m_label->set_text_color(GImGui->Style.Colors[selected ? ImGuiCol_Text : ImGuiCol_TextDisabled]);
+        m_label->set_font_type(selected ? Render::ImguiFontType::Bold : Render::ImguiFontType::Regular);
     }
 }
 
-void ProjectButton::on_selected_project_changed(size_t index) { set_selected(*m_state == index); }
+void ProjectButton::on_selected_project_changed(size_t index)
+{
+    set_selected(*m_state == index);
+}
 
 void ProjectButton::hovered_updated_internal()
 {
-    if (m_selected)
+    if (m_selected) {
         return;
+    }
+
     m_background->set_mode(
         hovered() ? ProjectButtonBackground::Border : ProjectButtonBackground::FilledRect
     );
