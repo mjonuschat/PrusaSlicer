44a4449b2d13ed934a296756fe5e929f03c8c065
YuSanka <yusanka@gmail.com>
2025-10-28 17:55:24 +0100

    Clipper use in PaintOnSupports gizmo
    + PaintOnSupportsDialog: Added callback for clipping ratio value change

--- src/slic3r-shared/include/Slic3r/App/Plater/PaintOnSupportsGizmo.hpp
+++ src/slic3r-shared/include/Slic3r/App/Plater/PaintOnSupportsGizmo.hpp
@@ -5,27 +5,56 @@
 #pragma once
 
 #include "Slic3r/App/Scene/IGizmo.hpp"
+#include "Slic3r/App/Scene/ClipperPresenter.hpp"
 #include "Slic3r/App/Yoga/Item.hpp"
+
+namespace Slic3r::App::Scene {
+class Clipper;
+} // namespace Slic3r::App::Scene
+
+namespace Slic3r::App::Render {
+class Device;
+} // namespace Slic3r::App::Render
+
+namespace Slic3r::Biz {
+class ProjectInteractor;
+} // namespace Slic3r::Biz
 
 namespace Slic3r::App::Plater {
 class PaintOnSupportsDialog;
+class PlaterScenePresenter;
 
 // Please implement me!
 class PaintOnSupportsGizmo : public Scene::IToolGizmo
 {
 public:
-    PaintOnSupportsGizmo();
+    PaintOnSupportsGizmo(
+        Render::Device& device,
+        PlaterScenePresenter& scene_presenter,
+        Biz::ProjectInteractor* project_interactor
+    );
 
     void on_activated() override;
     void on_deactivated() override;
 
+    void on_project_activated(size_t new_project_id) override;
+    void on_project_deactivated(size_t old_project_id) override;
+
     Scene::ToolType type() const override;
     Yoga::GizmoDialog* ui_dialog() override;
+
+    void provide_clipper(Scene::Clipper& clipper) override;
 
     Scene::GizmoActivationState on_mouse(Scene::GizmoEventContext& ctx, bool only_active) override;
 
 private:
     std::unique_ptr<PaintOnSupportsDialog> m_dialog;
+
+    Render::Device& m_device;
+    PlaterScenePresenter& m_scene_presenter;
+    Biz::ProjectInteractor* m_project_interactor{nullptr};
+
+    Scene::ClipperPresenter m_clipper_presenter;
 };
 
 } // namespace Slic3r::App::Plater
--- src/slic3r-shared/src/Slic3r/App/Plater/PaintOnSupportsGizmo.cpp
+++ src/slic3r-shared/src/Slic3r/App/Plater/PaintOnSupportsGizmo.cpp
@@ -6,24 +6,72 @@
 #include "Slic3r/App/Plater/PaintOnSupportsGizmo.hpp"
 
 #include "Slic3r/App/Plater/PaintOnSupportsDialog.hpp"
+#include "Slic3r/App/Plater/PlaterScenePresenter.hpp"
+
+#include "Slic3r/App/Scene/Clipper.hpp"
+#include "Slic3r/App/Scene/ClipperPresenter.hpp"
+#include "Slic3r/App/Render/Device.hpp"
+
+#include "Slic3r/Biz/ProjectInteractor.hpp"
 
 using namespace Slic3r::App::Yoga;
 
 namespace Slic3r::App::Plater {
 
-PaintOnSupportsGizmo::PaintOnSupportsGizmo()
+PaintOnSupportsGizmo::PaintOnSupportsGizmo(
+    Render::Device& device,
+    PlaterScenePresenter& scene_presenter,
+    Biz::ProjectInteractor* project_interactor
+) :
+    m_device(device),
+    m_scene_presenter(scene_presenter),
+    m_project_interactor(project_interactor)
 {
     m_dialog = std::make_unique<PaintOnSupportsDialog>();
+
+    m_dialog->callbacks().clipping_view_ratio_changed = [this](double ratio)
+    { m_clipper_presenter.set_position_by_ratio(ratio, true); };
 }
 
 void PaintOnSupportsGizmo::on_activated()
 {
+    const Biz::Scene::ObjectSelection& selection =
+        m_project_interactor->scene_interactor().object_selection();
 
+    if (selection.empty() || selection.mode != Slic3r::Biz::Scene::SelectionMode::Instance) {
+        on_deactivated();
+        // We can't perform a paint on supports for multiple objects simultaneously.
+        return;
+    }
+
+    Domain::Project& project = m_project_interactor->selected_project();
+
+    const Domain::ElementRef& element = selection.elements.front();
+    assert(element.volume_id == 0); // is object
+    Domain::ModelObject* selected_object = project.find_object_by_id(element.object_id);
+    Domain::ModelInstance* selected_instance =
+        project.find_instance_by_id(element.object_id, element.instance_id);
+    ASSERT(selected_instance && selected_object);
+
+    m_clipper_presenter.activate(&m_scene_presenter.scene(), selected_object, selected_instance);
+
+    m_clipper_presenter.set_behavior(true, true, 0.5);
+    m_clipper_presenter.set_position_by_ratio(0.5, false);
 }
 
 void PaintOnSupportsGizmo::on_deactivated()
 {
+    m_clipper_presenter.deactivate();
+}
 
+void PaintOnSupportsGizmo::on_project_activated(size_t new_project_id)
+{
+    on_activated();
+}
+
+void PaintOnSupportsGizmo::on_project_deactivated(size_t old_project_id)
+{
+    on_deactivated();
 }
 
 Scene::ToolType PaintOnSupportsGizmo::type() const
@@ -36,9 +84,16 @@ Yoga::GizmoDialog* PaintOnSupportsGizmo::ui_dialog()
     return m_dialog.get();
 }
 
-Scene::GizmoActivationState PaintOnSupportsGizmo::on_mouse(Scene::GizmoEventContext &ctx, bool only_active)
+void PaintOnSupportsGizmo::provide_clipper(Scene::Clipper& clipper)
+{
+    // fill clipper here
+    m_clipper_presenter = Scene::ClipperPresenter(&clipper, &m_device);
+}
+
+Scene::GizmoActivationState
+PaintOnSupportsGizmo::on_mouse(Scene::GizmoEventContext& ctx, bool only_active)
 {
     return Scene::GizmoActivationState::Inactive;
 }
 
-}
+} // namespace Slic3r::App::Plater
