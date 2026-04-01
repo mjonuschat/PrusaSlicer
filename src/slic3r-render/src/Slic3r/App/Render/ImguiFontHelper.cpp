2ab918d0ec71f6077fb5ad646b53ad88fa62be4e
Nikita Vanku <nikita.vanku@prusa3d.cz>
2026-04-01 15:05:59 +0200

    SPE-3481: Add NotoSans Thai font and diameter symbol
    Some of the languages and symbols are still missing. This commit
    adds support for NotoSans Thai symbols and diameter symbol.

--- src/slic3r-render/src/Slic3r/App/Render/ImguiFontHelper.cpp
+++ src/slic3r-render/src/Slic3r/App/Render/ImguiFontHelper.cpp
@@ -23,19 +23,13 @@ ImFont* ImguiFontHelper::font(Render::ImguiFontType type)
     return (it != m_fonts.end()) ? it->second : nullptr;
 }
 
-static ImFont* load_font(
-    const std::string& filename,
-    const std::string& filename_cjk,
-    const std::string& icon_font
-)
+static ImFont*
+load_font(const std::string& base_filename, const std::vector<std::string>& additional_fonts)
 {
     ImGuiIO& io            = ImGui::GetIO();
-    const std::string path = Slic3r::resources_dir() + "/fonts/" + filename;
+    const std::string path = Slic3r::resources_dir() + "/fonts/" + base_filename;
     ASSERT(boost::filesystem::exists(path), "font file does not exists");
-    const std::string path_cjk = Slic3r::resources_dir() + "/fonts/" + filename_cjk;
-    ASSERT(boost::filesystem::exists(path_cjk), "cjk font file does not exists");
-    const std::string path_icons = Slic3r::resources_dir() + "/fonts/" + icon_font;
-    ASSERT(boost::filesystem::exists(path_icons), "icon font file does not exists");
+
     ImFont* font = io.Fonts->AddFontFromFileTTF(path.c_str());
 
     if (font == nullptr) {
@@ -49,8 +43,11 @@ static ImFont* load_font(
     ImFontConfig config;
     config.MergeMode = true;
 
-    io.Fonts->AddFontFromFileTTF(path_cjk.c_str(), 0, &config);
-    io.Fonts->AddFontFromFileTTF(path_icons.c_str(), 0, &config);
+    for (const std::string& font : std::as_const(additional_fonts)) {
+        const std::string font_path = Slic3r::resources_dir() + "/fonts/" + font;
+        ASSERT(boost::filesystem::exists(font_path), "additional font file does not exists");
+        io.Fonts->AddFontFromFileTTF(font_path.c_str(), 0, &config);
+    }
 
     return font;
 }
@@ -60,12 +57,18 @@ void ImguiFontHelper::create_font_texture()
     ImGuiIO& io = ImGui::GetIO();
     io.Fonts->Clear();
 
-    m_fonts[ImguiFontType::Regular] =
-        load_font("NotoSans-Regular.ttf", "NotoSansCJK-Regular.ttc", "Slic3rIcons.ttf");
-    m_fonts[ImguiFontType::Bold] =
-        load_font("NotoSans-Bold.ttf", "NotoSansCJK-Bold.ttc", "Slic3rIcons.ttf");
-    m_fonts[ImguiFontType::Italic] =
-        load_font("NotoSans-Italic.ttf", "NotoSansCJK-Italic.ttc", "Slic3rIcons.ttf");
+    m_fonts[ImguiFontType::Regular] = load_font(
+        "NotoSans-Regular.ttf",
+        {"NotoSansCJK-Regular.ttc", "NotoSansThai-Regular.ttf", "Slic3rIcons.ttf"}
+    );
+    m_fonts[ImguiFontType::Bold] = load_font(
+        "NotoSans-Bold.ttf",
+        {"NotoSansCJK-Bold.ttc", "NotoSansThai-Bold.ttf", "Slic3rIcons.ttf"}
+    );
+    m_fonts[ImguiFontType::Italic] = load_font(
+        "NotoSans-Italic.ttf",
+        {"NotoSansCJK-Italic.ttc", "NotoSansThai-Regular.ttf", "Slic3rIcons.ttf"}
+    );
 }
 
 } // namespace Slic3r::App::Render
