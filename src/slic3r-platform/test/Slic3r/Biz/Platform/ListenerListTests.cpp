baf1fcc74ccf4dcf62fa39ca677dbaadd5c972f1
Martin Šach <martin.sachin@gmail.com>
2026-05-05 11:55:36 +0200

    Fix ListenerList

--- src/slic3r-platform/test/Slic3r/Biz/Platform/ListenerListTests.cpp
+++ src/slic3r-platform/test/Slic3r/Biz/Platform/ListenerListTests.cpp
@@ -39,3 +39,110 @@ TEST_CASE("Deleted listener can't be called", "")
     });
 }
 
+TEST_CASE("Added then removed listener can't be called", "")
+{
+    struct Interface
+    {
+        virtual void check() {};
+        virtual ~Interface() = default;
+    };
+
+    class Added : public Interface
+    {
+        void check() override
+        {
+            CHECK(false);
+        }
+    };
+
+    Interface i;
+    Added a;
+
+    Slic3r::Biz::ListenerList<Interface> listeners;
+    listeners.add(&i);
+    listeners.invoke(
+        [&](Interface* listener)
+        {
+            listeners.add(&a);
+            listeners.remove(&a);
+            listener->check();
+        }
+    );
+
+    size_t count = 0;
+    listeners.invoke(
+        [&](Interface* listener)
+        {
+            CHECK(listener == &i);
+            ++count;
+        }
+    );
+    CHECK(count == 1);
+}
+
+TEST_CASE("Duplicate add during invoke returns false", "")
+{
+    struct Interface
+    {
+        virtual ~Interface() = default;
+    };
+
+    Interface i;
+
+    Slic3r::Biz::ListenerList<Interface> listeners;
+    listeners.add(&i);
+
+    listeners.invoke(
+        [&](Interface*)
+        {
+            CHECK(listeners.add(&i) == false);
+            CHECK(listeners.add(&i) == false);
+        }
+    );
+}
+
+TEST_CASE("Added listener is called in same invoke", "")
+{
+    struct Interface
+    {
+        virtual void check() {};
+        virtual ~Interface() = default;
+    };
+
+    class Added : public Interface
+    {
+    public:
+        explicit Added(size_t* calls)
+            : m_calls{calls}
+        {
+        }
+
+        void check() override
+        {
+            ++(*m_calls);
+        }
+
+    private:
+        size_t* m_calls;
+    };
+
+    size_t calls = 0;
+    Interface i;
+    Added a{&calls};
+
+    Slic3r::Biz::ListenerList<Interface> listeners;
+    listeners.add(&i);
+
+    listeners.invoke(
+        [&](Interface* listener)
+        {
+            if (listener == &i) {
+                listeners.add(&a);
+            } else {
+                listener->check();
+            }
+        }
+    );
+
+    CHECK(calls == 1);
+}
