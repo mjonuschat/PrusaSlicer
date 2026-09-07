81aa0bd4613b61f56787dbb61cf64fcb21b90913
Morton Jonuschat <mjonuschat@gmail.com>
2026-09-07 15:50:14 -0700

    feat(toolchange): allow disabling automatic toolchange command emission

--- src/libslic3r/src/libslic3r/GCode.cpp
+++ src/libslic3r/src/libslic3r/GCode.cpp
@@ -4140,7 +4140,9 @@ std::string GCodeGenerator::set_extruder(unsigned int extruder_id, double print_
 
     // We inform the writer about what is happening, but we may not use the resulting gcode.
     std::string toolchange_command = m_writer.toolchange(extruder_id);
-    if (! custom_gcode_changes_tool(toolchange_gcode_parsed, m_writer.toolchange_prefix(), extruder_id))
+    if (config.get<bool>("autoemit_toolchange_commands")
+        && ! custom_gcode_changes_tool(toolchange_gcode_parsed, m_writer.toolchange_prefix(),
+                                        extruder_id))
         gcode += toolchange_command;
     else {
         // user provided his own toolchange gcode, no need to do anything
