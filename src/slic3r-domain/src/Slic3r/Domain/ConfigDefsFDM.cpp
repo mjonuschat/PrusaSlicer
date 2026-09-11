d2b10079a6d551e9e3b452fbe6dc54678ce77105
Morton Jonuschat <mjonuschat@gmail.com>
2026-09-10 17:24:36 -0700

    refactor(boss): rely on foundation's permanent extrusion_registry includes in GCode.cpp

--- src/slic3r-domain/src/Slic3r/Domain/ConfigDefsFDM.cpp
+++ src/slic3r-domain/src/Slic3r/Domain/ConfigDefsFDM.cpp
@@ -1658,7 +1658,7 @@ void fdm_config_init_fn(ConfigDefinitions& defs)
     def->label = L("First object layer over raft interface");
     def->option_group = ConfigItemDef::OptionGroup::Print_MotionDynamics_FirstLayerAcceleration;
     def->category = ConfigItemDef::Category::Print_MotionDynamics;
-    def->order = 1;
+    def->order = 10;
     def->gui_type = ConfigItemDef::GUIType::textfield;
     def->tooltip = L("This is the acceleration your printer will use for first layer of object above raft interface. Set zero "
                    "to disable acceleration control for first layer of object above raft interface.");
@@ -2062,6 +2062,7 @@ void fdm_config_init_fn(ConfigDefinitions& defs)
     def->label = L("Infill");
     def->option_group = ConfigItemDef::OptionGroup::Print_MotionDynamics_MainStructureAcceleration;
     def->category = ConfigItemDef::Category::Print_MotionDynamics;
+    def->order = 30;
     def->gui_type = ConfigItemDef::GUIType::textfield;
     def->tooltip = L("This is the acceleration your printer will use for infill. Set zero to disable "
                      "acceleration control for infill.");
@@ -2075,7 +2076,7 @@ void fdm_config_init_fn(ConfigDefinitions& defs)
     def->label = L("Solid infill");
     def->option_group = ConfigItemDef::OptionGroup::Print_MotionDynamics_MainStructureAcceleration;
     def->category = ConfigItemDef::Category::Print_MotionDynamics;
-    def->order = 4;
+    def->order = 40;
     def->gui_type = ConfigItemDef::GUIType::textfield;
     def->tooltip = L("This is the acceleration your printer will use for solid infill. Set zero to use "
                      "the value for infill.");
@@ -2089,7 +2090,7 @@ void fdm_config_init_fn(ConfigDefinitions& defs)
     def->label = L("Top solid infill");
     def->option_group = ConfigItemDef::OptionGroup::Print_MotionDynamics_MainStructureAcceleration;
     def->category = ConfigItemDef::Category::Print_MotionDynamics;
-    def->order = 5;
+    def->order = 50;
     def->gui_type = ConfigItemDef::GUIType::textfield;
     def->tooltip = L("This is the acceleration your printer will use for top solid infill. Set zero to use "
                      "the value for solid infill.");
@@ -2132,7 +2133,7 @@ void fdm_config_init_fn(ConfigDefinitions& defs)
     def->label        = L("Travel short distance acceleration");
     def->option_group = ConfigItemDef::OptionGroup::Print_MotionDynamics_TravelsAcceleration;
     def->category     = ConfigItemDef::Category::Print_MotionDynamics;
-    def->order = 1;
+    def->order = 10;
     def->gui_type     = ConfigItemDef::GUIType::textfield;
     def->tooltip      = L(
         "Acceleration used for short travel moves. Short travel distance is determined by the retract_before_travel setting."
@@ -2933,7 +2934,7 @@ void fdm_config_init_fn(ConfigDefinitions& defs)
     def->label = L("Perimeters");
     def->option_group = ConfigItemDef::OptionGroup::Print_MotionDynamics_MainStructureAcceleration;
     def->category = ConfigItemDef::Category::Print_MotionDynamics;
-    def->order = 3;
+    def->order = 20;
     def->gui_type = ConfigItemDef::GUIType::textfield;
     def->tooltip = L("This is the acceleration your printer will use for perimeters. "
                      "Set zero to disable acceleration control for perimeters.");
@@ -2946,6 +2947,7 @@ void fdm_config_init_fn(ConfigDefinitions& defs)
     def->label = L("External perimeters");
     def->option_group = ConfigItemDef::OptionGroup::Print_MotionDynamics_MainStructureAcceleration;
     def->category = ConfigItemDef::Category::Print_MotionDynamics;
+    def->order = 10;
     def->gui_type = ConfigItemDef::GUIType::textfield;
     def->tooltip = L("This is the acceleration your printer will use for external perimeters. "
                      "Set zero to use the value for perimeters.");
